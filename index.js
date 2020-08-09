5774d50779657e3da5b4c5bf0c8ac981f0ecbfe6
Tenyson Kwon <tenysonkwon@gmail.com>
2020-08-09 02:29:12 +0000

    request and error handling coordinates using api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -6,4 +7,14 @@ fetchMyIP((error, ip) => {
     return;
   }
   console.log('It worked! Returned IP:' , ip);
+});
+
+//review the following
+fetchCoordsByIP('162.245.144.188', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned Coords:' , coords);
 });
--- iss.js
+++ iss.js
@@ -22,4 +22,30 @@ const fetchMyIP = (callback) => {
   });
 };
 
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+    
+    if (error) return callback(error, null);
+    
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching coordinates: ${body}`), null);
+      return;
+    }
+  
+    const { latitude, longitude } = JSON.parse(body).data;//*important
+
+    callback(null, { latitude, longitude });
+
+  });
+};
+
+//to read the API output as object
+// request(`https://ipvigilante.com/70.54.64.15`, (error, response, body) => {
+//   const ipInfo1 = JSON.parse(body).data.latitude;
+//   const ipInfo2 = JSON.parse(body).data.longitude;
+//   console.log(ipInfo1, ipInfo2);
+// });
+
+
 module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
